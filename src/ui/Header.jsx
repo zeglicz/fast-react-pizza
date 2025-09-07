6b67783a5c6157b567ddf062e4b3a473cfc74c64
Przemyslaw Zeglicz <96738988+zeglicz@users.noreply.github.com>
2025-09-07 15:24:33 +0200

    feat: add AppLayout with Header, CartOverview and integrate into router

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast React Pizza Co.</Link>
+      <p>Przemek</p>
+    </header>
+  );
+}
+
+export default Header;
