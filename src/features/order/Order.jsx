cb335ce89f1984e715fe13c80a92943b81205e73
Przemyslaw Zeglicz <96738988+zeglicz@users.noreply.github.com>
2025-09-07 18:17:03 +0200

    feat: add create order form and action with React Router form handling

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -53,6 +53,7 @@ function Order() {
   );
 }
 
+// eslint-disable-next-line react-refresh/only-export-components
 export async function loader({ params }) {
   const order = await getOrder(params.orderId);
   return order;
